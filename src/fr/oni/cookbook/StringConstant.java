82f801dd1d1d2da40cc4eee8906279b8adbcc103
Daniel Chesters <daniel.chesters@gmail.com>
2013-08-16 16:38:00 +0200

    Forgotten StringConstant interface

---
+++ src/fr/oni/cookbook/StringConstant.java
@@ -0,0 +1,35 @@
+package fr.oni.cookbook;
+
+public interface StringConstant {
+ /*
+  * <string name="data_file">data.json</string>
+    <string name="tag_delete">delete</string>
+	<string name="tag_step">step</string>
+	<string name="tag_edit_ingredient">editIngredient</string>
+	<string name="tag_edit_step">editStep</string>
+	<string name="tag_data_write">Data write</string>
+	<string name="file_write_error">File write failed: </string>
+	<string name="tag_data_read">Data read</string>
+	<string name="file_read_error">Can not read file: </string>
+	<string name="file_read_not_found">File not found: </string>
+	<string name="key_recipe">recipe</string>
+	<string name="key_position_ingredient">posIngredient</string>
+	<string name="key_position_step">posStep</string>
+  */
+
+	String DATA_FILE = "data.json";
+	String TAG_DELETE = "delete";
+	String TAG_STEP = "step";
+	String TAG_EDIT_INGREDIENT = "editIngredient";
+	String TAG_EDIT_STEP = "editStep";
+	String TAG_DATA_WRITE = "Data write";
+	String FILE_WRITE_ERROR = "File write failed: ";
+	String TAG_DATA_READ = "Data read";
+	String FILE_READ_ERROR = "Can not read file: ";
+	String FILE_READ_NOT_FOUND = "File not found: ";
+	String KEY_RECIPE = "recipe";
+	String KEY_POSITION_INGREDIENT = "posIngredient";
+	String KEY_POSITION_STEP = "posStep";
+
+
+}
