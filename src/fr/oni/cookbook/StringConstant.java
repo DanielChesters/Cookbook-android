91c06a4672fd8bcf61762cb27992471079cacc6a
Daniel Chesters <daniel.chesters@gmail.com>
2014-02-13 20:42:47 +0100

    remove useless comment

--- src/fr/oni/cookbook/StringConstant.java
+++ src/fr/oni/cookbook/StringConstant.java
@@ -1,22 +1,6 @@
 package fr.oni.cookbook;
 
 public class StringConstant {
- /*
-  * <string name="data_file">data.json</string>
-    <string name="tag_delete">delete</string>
-    <string name="tag_step">step</string>
-    <string name="tag_edit_ingredient">editIngredient</string>
-    <string name="tag_edit_step">editStep</string>
-    <string name="tag_data_write">Data write</string>
-    <string name="file_write_error">File write failed: </string>
-    <string name="tag_data_read">Data read</string>
-    <string name="file_read_error">Can not read file: </string>
-    <string name="file_read_not_found">File not found: </string>
-    <string name="key_recipe">recipe</string>
-    <string name="key_position_ingredient">posIngredient</string>
-    <string name="key_position_step">posStep</string>
-  */
-
     public static final String DATA_FILE = "data.json";
     public static final String TAG_DELETE = "delete";
     public static final String TAG_STEP = "step";
@@ -34,6 +18,4 @@ public class StringConstant {
     private StringConstant() {
         // Nothing
     }
-
-
 }
