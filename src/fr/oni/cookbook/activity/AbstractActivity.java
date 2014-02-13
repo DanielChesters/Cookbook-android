f12fe80a9ea2edb3a62e6268d1f6b44de93dcf7b
Daniel Chesters <daniel.chesters@gmail.com>
2014-02-13 20:38:11 +0100

    refactor (again)

--- src/fr/oni/cookbook/activity/AbstractActivity.java
+++ src/fr/oni/cookbook/activity/AbstractActivity.java
@@ -1,5 +1,8 @@
 package fr.oni.cookbook.activity;
 
+import java.util.Arrays;
+import java.util.List;
+
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
@@ -8,7 +11,6 @@ import android.support.v7.app.ActionBar.Tab;
 import android.support.v7.app.ActionBar.TabListener;
 import android.support.v7.app.ActionBarActivity;
 import fr.oni.cookbook.R;
-import fr.oni.cookbook.Utilities;
 import fr.oni.cookbook.listener.TabChangeListener;
 import fr.oni.cookbook.model.Data;
 import fr.oni.cookbook.model.Recipe;
@@ -46,7 +48,27 @@ public abstract class AbstractActivity extends ActionBarActivity implements TabL
         int position = data.getPosition();
         Recipe recipe = data.getRecipes().get(position);
 
-        Utilities.commonSetup(actionBar, recipe, this);
+        actionBarSetup(actionBar, recipe);
+
+        final List<Integer> ids = Arrays.asList(new Integer[] {R.string.tab_name_recipe, R.string.tab_name_ingredients, R.string.tab_name_steps});
+        addTabs(actionBar, ids, this);
+    }
+
+    private void addTabs(final ActionBar actionBar, final List<Integer> stringIds, final TabListener tabListener) {
+        for (final int stringId : stringIds) {
+            actionBar.addTab(setTabListener(actionBar, stringId, tabListener));
+        }
+    }
+
+    private Tab setTabListener(final ActionBar actionBar, final int stringId, final TabListener tabListener) {
+        return actionBar.newTab().setText(stringId).setTabListener(tabListener);
+    }
+
+    private void actionBarSetup(final ActionBar actionBar, final Recipe recipe) {
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(recipe.getTitle());
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
 }
